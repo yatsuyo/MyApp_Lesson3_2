7a26df0bf4af255ddf7b02bbe981371b8af4c1f7
yatsuyo <hungry_soul_0122@yahoo.co.jp>
2016-07-01 22:54:22 +0900

    Lesson3 課題 再々提出 20160701

---
+++ app/src/main/java/com/example/admin/myapp_lesson3_2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.admin.myapp_lesson3_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 課題
+        Human human = new Human("ヤツ", 32, "アプリ開発");
+
+        human.say();
+        human.think();
+    }
+}
---
+++ app/src/main/java/com/example/admin/myapp_lesson3_2/Thinkable.java
@@ -0,0 +1,9 @@
+package com.example.admin.myapp_lesson3_2;
+
+/**
+ * Created by admin on 2016/07/01.
+ */
+public interface Thinkable {
+    public void say();
+    public void think();
+}
